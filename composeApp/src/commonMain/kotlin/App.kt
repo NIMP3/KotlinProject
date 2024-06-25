4def278826526fe9022253dd0d44c0738aa81970
Edwin Yovany <yovany.dev@gmail.com>
2024-06-24 20:51:14 -0500

    [BUILD] (Navigation): It implemented [Navigator - Scaffold]

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,27 +1,66 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Apps
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import data.ExpenseManager
-import data.ExpenseRepositoryImpl
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import data.TitleType
 import moe.tlaster.precompose.PreComposeApp
-import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
-import moe.tlaster.precompose.viewmodel.viewModel
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.rememberNavigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import presentation.ExpenseViewModel
-import presentation.ExpensesUiState
-import ui.ExpensesScreen
 
 @Composable
 @Preview
 fun App() {
     PreComposeApp {
-
-        val viewModel = viewModel(modelClass = ExpenseViewModel::class) {
-            ExpenseViewModel(ExpenseRepositoryImpl(ExpenseManager))
-        }
-        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
+        val colors = getColorsTheme()
         AppTheme {
-            ExpensesScreen(uiState) {}
+            val navigator = rememberNavigator()
+            val title = getTitle(navigator)
+
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(
+                        elevation = 0.dp,
+                        title = {
+                            Text(text = title.value,
+                                fontSize = 25.sp,
+                                color = colors.textColor)
+                        },
+                        backgroundColor = colors.backgroundColor,
+                        navigationIcon = {
+                            IconButton(onClick = {
+                                navigator.popBackStack()
+                            }) {
+                                Icon(
+                                    modifier = Modifier.padding(start = 16.dp),
+                                    imageVector = if (title == TitleType.DASHBOARD) Icons.Default.Apps else Icons.Default.ArrowBackIosNew,
+                                    tint = colors.textColor,
+                                    contentDescription = if (title == TitleType.DASHBOARD) "Dashboard" else "Back"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) {  }
         }
+    }
+}
+
+@Composable
+fun getTitle(navigator: Navigator): TitleType {
+    val route = navigator.currentEntry.collectAsState(null).value?.route?.route ?: "home"
+    return when(route) {
+        "expenseDetail/{id}" -> TitleType.EDIT
+        "addExpense/{id}" -> TitleType.ADD
+        else -> TitleType.DASHBOARD
     }
 }
---
+++ composeApp/src/commonMain/kotlin/navigation/Navigator.kt
@@ -0,0 +1,41 @@
+package navigation
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import data.ExpenseManager
+import data.ExpenseRepositoryImpl
+import getColorsTheme
+import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
+import moe.tlaster.precompose.navigation.NavHost
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.path
+import moe.tlaster.precompose.viewmodel.viewModel
+import presentation.ExpenseViewModel
+import ui.ExpensesScreen
+
+@Composable
+fun Navigation(navigator: Navigator) {
+    val colors = getColorsTheme()
+    val viewModel = viewModel(modelClass = ExpenseViewModel::class) {
+        ExpenseViewModel(ExpenseRepositoryImpl(ExpenseManager))
+    }
+    
+    NavHost(
+        modifier = Modifier.background(colors.backgroundColor),
+        navigator = navigator,
+        initialRoute = "home"
+    ) {
+        scene(route = "home") {
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+            ExpensesScreen(uiState) { expense ->
+                navigator.navigate("expenseDetail/${expense.id}")
+            }
+        }
+        
+        scene(route = "expenseDetail/{id}") {
+            val id = it.path<Long>("id")
+        }
+    }
+}
